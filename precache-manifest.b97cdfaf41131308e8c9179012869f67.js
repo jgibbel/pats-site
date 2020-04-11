3890c22be02477e7d091b0fdfe8b8983593bfac4
jgibbel <jamesgibbel@gmail.com>
2020-04-11 07:05:58 -0400

    Updates

---
+++ precache-manifest.b97cdfaf41131308e8c9179012869f67.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d09e0bbeedc973f860444638acae9106",
+    "url": "/pat/index.html"
+  },
+  {
+    "revision": "03adb8d48186f290d6e6",
+    "url": "/pat/static/css/main.aa6c4b2b.chunk.css"
+  },
+  {
+    "revision": "c25ac3210c623a3366bf",
+    "url": "/pat/static/js/2.92145041.chunk.js"
+  },
+  {
+    "revision": "4fdc00fbc861a978a4476cc8b3c4086f",
+    "url": "/pat/static/js/2.92145041.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03adb8d48186f290d6e6",
+    "url": "/pat/static/js/main.54950319.chunk.js"
+  },
+  {
+    "revision": "276a37e4816fc969208e",
+    "url": "/pat/static/js/runtime-main.57aabb9b.js"
+  },
+  {
+    "revision": "b314d1ae2b35bb183d7106ca011524bc",
+    "url": "/pat/static/media/back-arrow.b314d1ae.svg"
+  },
+  {
+    "revision": "076dbacdd1afe7af4c7428020c318a65",
+    "url": "/pat/static/media/forward-arrow.076dbacd.svg"
+  }
+]);
