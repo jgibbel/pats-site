d9407ccc775ec3eb3f04fd2fbddde62a79de0d4c
jgibbel <jamesgibbel@gmail.com>
2020-04-14 17:00:39 -0400

    edits

--- src/Contact.js
+++ src/Contact.js
@@ -7,7 +7,10 @@ function Contact() {
             <p><a href="mailto:write2me@patcummings.com">write2me@patcummings.com</a></p>
             {/* <p>Instagram <a href="mailto:write2me@patcummings.com">link</a></p>
             <p>Facebook <a href="mailto:write2me@patcummings.com">link</a></p> */}
-
+            <br /><br />
+            <p><b>Marietta B. Zacker</b></p>
+            <p>gallt & zacker Literary Agency</p>
+            <p><a href="marietta@galltzacker.com">marietta@galltzacker.com</a></p>
         </div>
   );
 }
